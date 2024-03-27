8e0e9538ea227109c4b5e83f5b837e81d1065d02
Diego Cardenas <diego.cardenast@gmail.com>
2024-03-27 09:47:02 +0000

    feat(database): connect to elephanSQL database

--- plastilecor_portal/settings.py
+++ plastilecor_portal/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ps&utie%39n1a(m_#ys1ui7rd)jkmaccnlf2*!ezeoskjot&45'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-diegocarden-pp4plastile-dl4pq8wxrog.ws-eu110.gitpod.io','.herokuapp.com']
 
@@ -73,11 +77,15 @@ WSGI_APPLICATION = 'plastilecor_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
