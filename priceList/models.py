43cc6306b8476ed0abd3c9cc6aabd92c85151822
Diego Cardenas <diego.cardenast@gmail.com>
2024-03-29 15:26:22 +0000

    feat: add model code ListItem

--- priceList/models.py
+++ priceList/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ListItem (model.Model):
+    productName = models.CharField(max_length=200, unique=True)
+    productCode = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="list_items"
+    )
+    dimensions = models.CharField(max_length=200, unique=False)
+    price = models.Integer(max_length=200, unique=False)
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    lastModified_on = models.DateTimeField(auto_now_add=True, editable=True)
+
+    # Source: https://stackoverflow.com/questions/4754485/dry-way-to-add-created-modified-by-and-time
+    def save(self):
+        if self.id:
+            self.lastModified_on = datetime.now()
+        else:
+            self.created_on = datetime.now()
+        super(MyModel,self).save()
