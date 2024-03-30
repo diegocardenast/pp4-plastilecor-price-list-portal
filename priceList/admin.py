f5c2b700b785fcb344411e9fff142c5bf2b74cfd
Diego Cardenas <diego.cardenast@gmail.com>
2024-03-30 11:17:51 +0000

    fix: register ListItem model to priceList

--- priceList/admin.py
+++ priceList/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import priceList
+from .models import ListItem 
 
 # Register your models here.
-admin.site.register(priceList)
+admin.site.register(ListItem)
 
