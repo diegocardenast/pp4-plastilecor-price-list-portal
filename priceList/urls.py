8b91151f48fe68f19d80b80c9cddf7562641e7df
Diego Cardenas <diego.cardenast@gmail.com>
2024-04-14 18:25:47 +0000

    feat: add product detail template

--- priceList/urls.py
+++ priceList/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('<productCode>/', views.product_detail, name='product_detail'),
 ]
--- priceList/views.py
+++ priceList/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 
 # from django.http import HttpResponse -- NOT NEEDED AFTER CREATING THE VIEWS
 from django.views import generic
 from .models import ListItem
-
 
 
 #def my_priceList(request):
@@ -15,3 +14,26 @@ class PostList(generic.ListView):
     # template_name = "priceList/price_list.html" -- NOT NEEDED AFTER CREATING INDEX.html
     template_name = "priceList/index.html"
     paginate_by = 6
+
+
+def product_detail(request, productCode):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    productDetail = get_object_or_404(ListItem, productCode=productCode)
+
+    return render(
+        request,
+        "priceList/product_detail.html",
+        {"product": productDetail},
+    )
