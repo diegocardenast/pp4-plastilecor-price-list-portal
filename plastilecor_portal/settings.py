de21152f71736d43649db920d22910d95894770a
Diego Cardenas <diego.cardenast@gmail.com>
2024-05-15 08:50:15 +0000

    feat: add institutional color to base file and include logo

--- plastilecor_portal/settings.py
+++ plastilecor_portal/settings.py
@@ -29,9 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-diegocarden-pp4plastile-dl4pq8wxrog.ws-eu110.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-diegocarden-pp4plastile-dl4pq8wxrog.ws-us110.gitpod.io','.herokuapp.com']
 
 # Application definition
 
