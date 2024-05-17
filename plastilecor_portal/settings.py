90908bb5ed970f452e9624199814ed9be7036759
Diego Cardenas <diego.cardenast@gmail.com>
2024-05-17 23:56:03 +0000

    feat: adjust buttons color and price list table aspect

--- plastilecor_portal/settings.py
+++ plastilecor_portal/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-diegocarden-pp4plastile-dl4pq8wxrog.ws-us110.gitpod.io','8000-diegocarden-pp4plastile-dl4pq8wxrog.ws-eu111.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-diegocarden-pp4plastile-dl4pq8wxrog.ws-us110.gitpod.io','8000-diegocarden-pp4plastile-dl4pq8wxrog.ws-eu111.gitpod.io','8000-diegocarden-pp4plastile-dl4pq8wxrog.ws-eu114.gitpod.io','.herokuapp.com']
 
 # Application definition
 
