a356c0303c0fb4abb25998494446229157a54e51
Diego Cardenas <diego.cardenast@gmail.com>
2024-05-20 12:20:53 +0000

    fix: adjusting contact files with W3C and Python Linter tools

--- contactUs/models.py
+++ contactUs/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class ContactUsRequest(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
--- contactUs/views.py
+++ contactUs/views.py
@@ -4,14 +4,20 @@ from .models import ContactUsRequest
 from .forms import ContactForm
 
 # Create your views here.
-def contact_us(request):
 
-    #POST handling
+
+def contact_us(request):
+    # POST handling.
     if request.method == "POST":
         contact_form = ContactForm(data=request.POST)
         if contact_form.is_valid():
             contact_form.save()
-            messages.add_message(request, messages.SUCCESS, "Request received! We will be in contact with you as soon as we can.")
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Request received! We will be in contact with you as"
+                - " soon as we can."
+            )
 
     contact_form = ContactForm()
 
